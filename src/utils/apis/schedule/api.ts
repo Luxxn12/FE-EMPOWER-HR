ffd3c2c0949b12c8a743431aa73a4e2b53f8e3cd
Berykwn <berykurniawan46@gmail.com>
2024-07-28 14:44:06 +0700

    update pagination

---
+++ src/utils/apis/schedule/api.ts
@@ -0,0 +1,17 @@
+import { Response } from "@/utils/types/apis";
+import { openAPI } from "../axiosWithConfig";
+import { ISchedule } from "./type";
+
+export const getSchedule = async () => {
+  try {
+    const resp = await openAPI.get("/schedule");
+
+    return resp.data as Response<ISchedule[]>;
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      const { message } = error.response.data;
+      throw new Error(message);
+    }
+    throw error;
+  }
+};
